47468c819bc61a38576a56c6b435fec499762257
b5user45 <b5user45@rpsconsulting.in>
2024-12-30 23:41:02 +0530

    UI_Animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -61,3 +61,23 @@ const headerActive = function () {
 }
 
 addEventOnElem(window, "scroll", headerActive);
+
+
+
+
+function reveal() {
+  const reveals = document.querySelectorAll('.reveal');
+  
+  reveals.forEach(element => {
+    const windowHeight = window.innerHeight;
+    const elementTop = element.getBoundingClientRect().top;
+    const elementVisible = 150;
+    
+    if (elementTop < windowHeight - elementVisible) {
+      element.classList.add('active');
+    }
+  });
+}
+
+window.addEventListener('scroll', reveal);
+reveal();
